22430a1b30b0ad0f54ea6ccdfbfdb6aaf3846d55
Nikola Obradović <obradovicnikola009@gmail.com>
2022-05-02 12:26:27 +0200

    added halls display

--- pages/admin/halls.vue
+++ pages/admin/halls.vue
@@ -5,26 +5,68 @@
     <new-hall></new-hall>
 
     <div class="container">
-      <span>list halls...</span>
+      <v-data-table
+        :headers="[
+          { text: 'id', value: 'id' },
+          { text: 'Naziv', value: 'name' },
+          { text: 'Seats Ground Floor', value: 'displayDto.seatsGroundFloor' },
+          { text: 'Seats Gallery Left', value: 'displayDto.seatsGalleryLeft' },
+          {
+            text: 'Seats Gallery Right',
+            value: 'displayDto.seatsGalleryRight',
+          },
+          { text: 'control', value: 'control' },
+        ]"
+        :items="halls"
+        :items-per-page="5"
+        class="elevation-1"
+        :search="tableSearch"
+      >
+        <template #top>
+          <v-text-field
+            v-model="tableSearch"
+            label="Search"
+            class="mx-4"
+          ></v-text-field>
+        </template>
+        <template #[`item.control`]="{ item }">
+          <v-btn
+            color="error"
+            style="width: 100px"
+            :loading="item.buttonLoading"
+            @click="enableDisableHall(item.id, true)"
+          >
+            <v-icon small class="mr-2"> mdi-delete </v-icon>
+            Delete
+          </v-btn>
+          <!-- <nuxt-link :to="`/profile/${item.id}`">
+            <v-icon small class="mr-2"> mdi-account-arrow-right </v-icon>
+          </nuxt-link> -->
+        </template>
+      </v-data-table>
     </div>
   </div>
 </template>
 
 <script>
-// import NewHall from '../../components/Modals/NewHall.vue'
-
 const name = 'Halls'
 const layout = 'admin'
 
 const middleware = ['auth-and-admin']
 
-// const components = {
-//   NewHall,
-// }
+const data = () => ({
+  halls: [],
+  tableSearch: '',
+  buttonLoading: false,
+})
+
 export default {
   name,
-  // components,
   layout,
   middleware,
+  data,
+  async fetch() {
+    this.halls = await this.$axios.$get('api/halls')
+  },
 }
 </script>
